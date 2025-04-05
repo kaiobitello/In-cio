b685565e8680ae8a61698bc8af844bbf861ceb78
Kaiozxc <Kaiuz0000000@gmail.com>
2025-04-05 18:13:35 -0300

    Alterações AQL

--- views/models/db.js
+++ views/models/db.js
@@ -1,8 +1,11 @@
 import { Sequelize } from "sequelize";
-const sequelize = new Sequelize('form', 'root', '01039119kaio!', {
-    host: "localhost",
-    dialect: "mysql"
+const sequelize = new Sequelize('inicio-db', 'root', '01039119kaio!', {
+    host: "postgresql://inicio_db_user:RhQ14MWglQXe5lDpITZbVeFE5EjxULmP@dpg-cvopn6vgi27c73astd10-a/inicio_db",
+    port: 5432,
+    dialect: "postgres"
+
 });
+
 
 export default {
     Sequelize: Sequelize,
